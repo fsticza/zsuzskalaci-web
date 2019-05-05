9a05b7b0a617e0ce89d37f7ef68ad389165b6853
Ferenc Sticza <ferenc.sticza@gmail.com>
2019-05-05 22:18:15 +0200

    deploy

---
+++ precache-manifest.55d883e5ec743f1be71c7bbc42afe28f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8c085ec65591533f9e5",
+    "url": "/css/app.56225221.css"
+  },
+  {
+    "revision": "d8c085ec65591533f9e5",
+    "url": "/js/app.0bd8e72f.js"
+  },
+  {
+    "revision": "70a8ee0d192c52d3564b",
+    "url": "/js/chunk-vendors.4493643d.js"
+  },
+  {
+    "revision": "0e7cc7b263a20eedd9c57e4b0a364b43",
+    "url": "/img/03_nyaralas.0e7cc7b2.jpg"
+  },
+  {
+    "revision": "03f9ad8c53110d9585e9341da05b8db4",
+    "url": "/img/04_osszekoltozes.03f9ad8c.jpg"
+  },
+  {
+    "revision": "b5b68d5c43b61e177bb13b62914dfaf6",
+    "url": "/img/05_eljegyzes.b5b68d5c.jpg"
+  },
+  {
+    "revision": "691368185b618125558e4c69641b029e",
+    "url": "/img/01_itsamatch.69136818.jpg"
+  },
+  {
+    "revision": "1a2cd220638abb62caf791131529deaf",
+    "url": "/img/laci.1a2cd220.jpg"
+  },
+  {
+    "revision": "5d4a18c0f681cc9b83f459ef1218aa6b",
+    "url": "/img/06_esmost.5d4a18c0.jpg"
+  },
+  {
+    "revision": "38c5f8281438118a36f2a03c79a6444c",
+    "url": "/img/countdown-bg.38c5f828.jpg"
+  },
+  {
+    "revision": "4bdc5c59d33e212322c8cb6606f024dd",
+    "url": "/img/header-photo.4bdc5c59.jpg"
+  },
+  {
+    "revision": "309c5666b8ff072a743d29b040a30ac0",
+    "url": "/img/02_elsorandi.309c5666.jpg"
+  },
+  {
+    "revision": "1c03f0fd27c0bb22b9dc75f716d37968",
+    "url": "/img/zsuzska.1c03f0fd.jpg"
+  },
+  {
+    "revision": "96be6aa1281573678ecafc696a244651",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
